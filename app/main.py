7e44bb15e6cb9dd33cac65526764e55cc4ae5bbe
parallels <denis.sergeef1@gmail.com>
2025-11-03 09:05:11 +0400

    docs: updated

--- app/main.py
+++ app/main.py
@@ -29,6 +29,7 @@ app.include_router(reminders.router)
 
 # --------------------------------------------------------------------------------
 # Static Files
+
 # --------------------------------------------------------------------------------
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
